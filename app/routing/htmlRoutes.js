b9e5d8e4e9a3341b365cf46622b37dec08aba03f
Tony Salvati <tsal2184@gmail.com>
2018-11-05 17:56:29 -0700

    Updated FriendFinder

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,11 +2,11 @@ var path = require('path');
 
 module.exports = function (app) {
 	app.get('/survey', function(req, res) {
-    	res.sendFile(path.join(__dirname + '/../public/survey.html'));
+    	res.sendFile(path.join(__dirname + '/../survey.html'));
 	});
 
 
 	app.get("/", function(req, res) {
-    	res.sendFile(path.join(__dirname + '/../public/home.html'));
+    	res.sendFile(path.join(__dirname + '/../home.html'));
 	});
 }
